972a0759372d1f9834fc5b4140f88f797a87e37b
Aindi <send_ayndi@mail.ru>
2019-04-14 18:08:23 +0300

    Это наш первый коммит

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let persone = {
+    name: "John",
+    age: 33,
+    isMarried: false
+};
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+//console.log(arr[0]);
+
+//alert("Hello World!");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+//let answer = prompt("Есть ли вам 18 лет?", "Да");
+//console.log(typeof(undefined));
+
+console.log(3 + " - object");
