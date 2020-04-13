d50c37723d88b4aa202278270a16f457f963fdb1
Aindi <send_ayndi@mail.ru>
2020-04-13 18:45:35 +0300

    завершил задание 9

--- zud/admin/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('../include/autoload.php');
-$main = new Main(); // instantiating the Main class.
-$main->run(); // calls run method.
-
--- zud/admin/pages/logout.php
+++
@@ -1,3 +0,0 @@
-<?php
-Login::logout(Login::$admin_login);
-Login::noAccessAdmin();
